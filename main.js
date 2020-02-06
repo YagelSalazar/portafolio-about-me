dafb4b8eaf2bba6eef08070607efce5ae547d700
YagelSalazar <yagels.xd@gmail.com>
2020-02-05 22:04:55 -0600

    ejercicio 2 para indicador alert primer prueba de javascript implementada listo

---
+++ main.js
@@ -0,0 +1,18 @@
+function uno(){
+    alert(1)
+}
+function dos(){
+    alert(2)
+}
+function tres(){
+    alert(3)
+}
+function cuatro(){
+    alert(4)
+}
+function cinco(){
+    alert(5)
+}
+function seis(){
+    alert(6)
+}
